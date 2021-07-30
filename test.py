ee75324c52eaee189d96d39b485b0bf1f65d8332
Aaroh Gokhale <gokhaleaaroh@gmail.com>
2021-07-30 21:24:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -82,10 +82,3 @@ print("Lets try the to_string function that outputs the polynomial currently bei
       "vector:")
 
 print(nm.to_string())
-
-print("+++++++++++++leo+++++++")
-del NewtonsMethod
-import newtonsmethod # noqa
-object = newtonsmethod.NewtonsMethod([1, 0, -2])
-
-print(object.eval_poly(2))
